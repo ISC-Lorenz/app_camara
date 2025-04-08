9236b3029e699e98320ef38fbf65c2234e01745e
ISC-Lorenz <isc.lorenzovr@gmial.com>
2025-04-08 00:12:25 -0600

    feat: Models created for account

---
+++ lib/practica_bloc/models/account_properties.dart
@@ -0,0 +1,3 @@
+//Propiedades de la cuenta
+//Se agregaran las propiedades de la cuenta que se necesiten para el funcionamiento de la aplicacion
+//Como el saldo, el nombre de la cuenta, el id de la cuenta, etc
